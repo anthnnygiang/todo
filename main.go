16329086fd29fcbffdb31b2d18ea2dfc17b2b930
Anthony Giang <anthnnygiang@icloud.com>
2025-08-03 12:39:33 +1000

    rearchitect

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/alecthomas/kong"
 )
 
 const Reset = "\033[0m"
+const Black = "\033[30m"
 const Red = "\033[31m"
 const Green = "\033[32m"
 const Yellow = "\033[33m"
@@ -18,21 +20,28 @@ const Blue = "\033[34m"
 const Magenta = "\033[35m"
 const Cyan = "\033[36m"
 const Gray = "\033[37m"
+const BrightBlack = "\033[90m"
+const BrightRed = "\033[91m"
+const BrightGreen = "\033[92m"
+const BrightYellow = "\033[93m"
+const BrightBlue = "\033[94m"
+const BrightMagenta = "\033[95m"
+const BrightCyan = "\033[96m"
 const White = "\033[97m"
 
-var CLI struct {
-	Ls struct{} `cmd:"" help:"List all todo items."`
+const Bold = "\033[1m"
+const Dim = "\033[2m"
+const Italic = "\033[3m"
+const Underline = "\033[4m"
+const Blink = "\033[5m"
+const Reverse = "\033[7m"
+const Hidden = "\033[8m"
+const StrikeThrough = "\033[9m"
 
-	Add struct {
-		Title string `help:"Title of the todo item." arg:""`
-	} `cmd:"" help:"Add a todo item."`
+var repositoryPath = "dev/.zzz/todo"
+var todosFile = "todos.txt"
 
-	Rm struct {
-		Number []string `help:"Todo items to complete." arg:"" optional:""`
-	} `cmd:"" help:"Complete one or more todo items. If no numbers are provided, complete all todo items."`
-}
-
-type CLI2 struct {
+type CLI struct {
 	Ls  LsCmd  `cmd:"" help:"List all todo items."`
 	Add AddCmd `cmd:"" help:"Add a todo item."`
 	Rm  RmCmd  `cmd:"" help:"Complete one or more todo items. If no numbers are provided, complete all todo items."`
@@ -43,6 +52,16 @@ type AddCmd struct {
 }
 type RmCmd struct {
 	Number []string `help:"Todo items to complete." arg:"" optional:""`
+}
+
+func main() {
+	// errors are handled automatically
+	CLISpec := CLI{}
+	ctx := kong.Parse(&CLISpec)
+	if err := ctx.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
 func (c *LsCmd) Run() error {
@@ -63,8 +82,7 @@ func (c *AddCmd) Run() error {
 
 	input := c.Title
 	bytes := make([]byte, 0)
-	_, err = file.Write(fmt.Appendf(bytes, "%s\n", input))
-	if err != nil {
+	if _, err = file.Write(fmt.Appendf(bytes, "%s\n", input)); err != nil {
 		return err
 	}
 	list(file)
@@ -72,67 +90,59 @@ func (c *AddCmd) Run() error {
 	return nil
 }
 
-var repositoryPath = "dev/.zzz/todo"
-var todosFile = "todos.txt"
+func (c *RmCmd) Run() error {
+	file, err := openFile()
+	if err != nil {
+		return err
+	}
+	// if no numbers are provided, complete all todo items
+	if len(c.Number) == 0 {
+		file.Truncate(0)
+		return nil
+	}
+
+	// create a map of numbers
+	numbersMap := make(map[int]bool)
+	for _, n := range c.Number {
+		num, err := strconv.Atoi(n)
+		if err != nil {
+			return err
+		}
+		numbersMap[num] = true
+	}
+
+	// read the file and keep todos that are not in numbersMap
+	var remainingTodos []string
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	for i := 1; fileScanner.Scan(); i++ {
+		if numbersMap[i] {
+			continue
+		}
+		remainingTodos = append(remainingTodos, fileScanner.Text())
+	}
+
+	if err = file.Truncate(0); err != nil {
+		return err
+	}
+	if _, err = file.Seek(0, io.SeekStart); err != nil {
+		return err
+	}
+
+	for _, line := range remainingTodos {
+		bytes := make([]byte, 0)
+		_, err := file.Write(fmt.Appendf(bytes, "%s\n", line))
+		if err != nil {
+			return err
+		}
+	}
+	list(file)
+	defer closeFile(file)
+	return nil
+}
 
 func run() {
 	fmt.Print("TODO.")
-}
-
-func main() {
-	// errors are handled automatically
-	CLISpec := CLI2{}
-	ctx := kong.Parse(&CLISpec)
-	if err := ctx.Run(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	// switch ctx.Command() {
-	// case "ls":
-	// 	list(file)
-
-	// case "add <title>":
-	// 	input := CLI.Add.Title
-	// 	bytes := make([]byte, 0)
-	// 	_, err = file.Write(fmt.Appendf(bytes, "%s\n", input))
-	// 	check(err)
-	// 	list(file)
-
-	// case "rm":
-	// 	err := os.Truncate(filename, 0)
-	// 	check(err)
-
-	// case "rm <number>":
-	// 	// Create a map of numbers to check quickly.
-	// 	numbersMap := make(map[int]bool)
-	// 	for _, n := range CLI.Rm.Number {
-	// 		num, err := strconv.Atoi(n)
-	// 		check(err)
-	// 		numbersMap[num] = true
-	// 	}
-
-	// 	// Read the file and skip lines that are in numbersMap.
-	// 	var remainingTodos []string
-	// 	fileScanner := bufio.NewScanner(file)
-	// 	fileScanner.Split(bufio.ScanLines)
-	// 	for i := 1; fileScanner.Scan(); i++ {
-	// 		if numbersMap[i] {
-	// 			continue
-	// 		}
-	// 		remainingTodos = append(remainingTodos, fileScanner.Text())
-	// 	}
-
-	// 	err = file.Truncate(0)
-	// 	check(err)
-
-	// 	for _, line := range remainingTodos {
-	// 		bytes := make([]byte, 0)
-	// 		_, err := file.Write(fmt.Appendf(bytes, "%s\n", line))
-	// 		check(err)
-	// 	}
-	// 	list(file)
-	// }
 }
 
 // list prints all items in the file.
@@ -149,15 +159,16 @@ func list(file *os.File) error {
 		todoLines = append(todoLines, fileScanner.Text())
 	}
 	if len(todoLines) == 0 {
-		fmt.Printf("%sAll done!%s\n", Green, Reset)
+		fmt.Printf("%sall done!%s\n", Green, Reset)
 	}
 	for i, line := range todoLines {
-		fmt.Printf("%s%d.%s %s\n", Green, i+1, Reset, line)
+		fmt.Printf("%s%s%d.%s %s\n", Green, Bold, i+1, Reset, line)
 	}
 	return nil
 }
 
 func openFile() (*os.File, error) {
+	// open Todo file
 	home, err := os.UserHomeDir()
 	if err != nil {
 		return nil, err
@@ -165,7 +176,7 @@ func openFile() (*os.File, error) {
 	filename := filepath.Join(home, repositoryPath, todosFile)
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		fmt.Printf("%s%s does not exist.%s\n", Red, todosFile, Reset)
-		fmt.Printf("%sCreating %s...%s\n", Red, todosFile, Reset)
+		fmt.Printf("%screating %s...%s\n", BrightYellow, todosFile, Reset)
 	}
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
